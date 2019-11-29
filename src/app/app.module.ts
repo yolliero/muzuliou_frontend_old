82219b89c2a9458d22df2da8c27c2688cc210324
OLLIEROU <yves.ollierou@capgemini.com>
2019-11-29 14:59:49 +0100

    add custom themes for angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
 import { HomeRoutingModule } from './home/home-routing.module';
 import { HomeModule } from './home/HomeModule';
+import { AddEcmeComponent } from './add-ecme/add-ecme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AddEcmeComponent
   ],
   imports: [
     BrowserModule,
